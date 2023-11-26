f811b530508b37648efca82c0e14514ce3030893
tlmcbee <tlmcbeejr@gmail.com>
2023-11-26 14:54:25 -0800

    Initial commit

---
+++ models/skill.js
@@ -0,0 +1,7 @@
+const skills = [
+  {id: 123, skill: 'Javascript', level: 4},
+  {id: 456, skill: 'HTML', level: 7},
+  {id: 789, skill: 'CSS', level: 7},
+  {id: 1011, skill: 'Node.js', level: 2},
+  {id: 1213, skill: 'Express', level: 2},
+]
